cef2f8c2dc825784d63606f687df73907582d634
marcusloy77 <marcus.loy.77@gmail.com>
2022-04-25 01:29:44 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,198 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const gravity = 0.5
+const maxVelocity = 7
+
+
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 30
+        this.height = 30
+        this.grounded = false
+        this.jumps = 0
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y, this.width, this.height)
+    }
+    update() {
+        
+        
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+
+        
+        if(this.position.y + this.height + this.velocity.y <= canvas.height){
+            this.velocity.y += gravity
+        }
+        else{
+            this.velocity.y = 0
+            this.grounded = true
+        }
+        this.draw()
+
+
+        
+    }
+
+}
+
+//platform construct
+class Platform {
+    constructor({x,y}) {
+        this.position = {
+            x,
+            y
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+
+
+
+const player = new Player()
+const platforms = [new Platform({x : 200, y : 50}), new Platform({x : 400, y : 20})]
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+    up: {
+        pressed: false
+    },
+    down: {
+        pressed : false
+    }
+}
+
+let scrollOffset = 0
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0,0,canvas.width,canvas.height)
+    player.update()
+
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
+
+    //player left right movement
+    if(keys.right.pressed && player.position.x < 400){
+        player.velocity.x = 5
+        scrollOffset += 5
+    }
+    else if(keys.left.pressed && player.position.x > 100){
+        player.velocity.x = -5
+        scrollOffset -=5
+    }
+    else {
+        player.velocity.x = 0
+        if (keys.right.pressed){
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+            })
+        
+            
+        }
+        else if (keys.left.pressed){
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+            })
+        }
+    }
+
+    //jumping code
+
+    
+
+    
+    platforms.forEach((platform) =>{
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
+            player.velocity.y = 0
+            player.grounded = true
+        }
+    })
+    if(keys.up.pressed && player.grounded){
+        player.velocity.y = -20
+    }
+
+    if (player.grounded){
+        player.jumps = 0
+    }
+
+    //platform collision
+    
+    
+    player.grounded = false
+
+}
+
+animate()
+
+
+//event listeners for movement
+addEventListener('keydown', ({keyCode}) => {
+    switch (keyCode){
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break
+        case 83:
+            console.log('down')
+            keys.down.pressed = true
+            break
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+        case 87:
+            console.log('up')
+            keys.up.pressed = true
+
+            break
+    }
+})
+addEventListener('keyup', ({keyCode}) => {
+    switch (keyCode){
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break
+        case 83:
+            console.log('down')
+            keys.down.pressed = false
+            break
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break
+        case 87:
+            console.log('up')
+            keys.up.pressed = false
+            break
+    }
+})
